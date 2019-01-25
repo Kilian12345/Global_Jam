5e6f0c754ea4c18285f16859044d7a5869cdaa21
Léonard Gombert <epoc4oftime@gmail.com>
2019-01-25 22:11:07 +0100

    PlayerController working

---
+++ Neonizer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float maxSpeed;
+    public float acceleration;
+    public float steering;
+
+    private Rigidbody2D rb;
+    float currentSpeed;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        // Get input
+        float horizontal = -Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        // Calculate speed from input and acceleration (transform.up is forward)
+        Vector2 speed = transform.up * (vertical * acceleration);
+        rb.AddForce(speed);
+
+        // Create car rotation
+        float direction = Vector2.Dot(rb.velocity, rb.GetRelativeVector(Vector2.up));
+        if (direction >= 0.0f)
+        {
+            rb.rotation += horizontal * steering * (rb.velocity.magnitude / maxSpeed);
+        }
+        else
+        {
+            rb.rotation -= horizontal * steering * (rb.velocity.magnitude / maxSpeed);
+        }
+
+        // Change velocity based on rotation
+        float driftForce = Vector2.Dot(rb.velocity, rb.GetRelativeVector(Vector2.left)) * 2.0f;
+        Vector2 relativeForce = Vector2.right * driftForce;
+        Debug.DrawLine(rb.position, rb.GetRelativePoint(relativeForce), Color.green);
+        rb.AddForce(rb.GetRelativeVector(relativeForce));
+
+        // Force max speed limit
+        if (rb.velocity.magnitude > maxSpeed)
+        {
+            rb.velocity = rb.velocity.normalized * maxSpeed;
+        }
+        currentSpeed = rb.velocity.magnitude;
+    }
+
+}
