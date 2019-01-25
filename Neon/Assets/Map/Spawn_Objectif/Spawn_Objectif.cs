428651d7903edcecfa5c58234ecd29f2596a9f35
dEFAULT <allairekilian@gmail.com>
2019-01-25 22:50:55 +0100

    Map_10%

---
+++ Neon/Assets/Map/Spawn_Objectif/Spawn_Objectif.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn_Objectif : MonoBehaviour
+{
+    [SerializeField]
+    Transform limite1;
+    [SerializeField]
+    Transform limite2;
+}
