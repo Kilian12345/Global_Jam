1e8f9d4302338fa1570837a2ee06d8a568e21e1b
Léonard Gombert <epoc4oftime@gmail.com>
2019-01-25 23:19:31 +0100

    Tests Scripts PlayerCont

---
+++ Neonizer/Assets/Scripts/PlayerController3.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController3 : MonoBehaviour
+{
+    #region Variables
+
+    public float maxSpeed;
+    public float acceleration;
+
+    float currentSpeed;
+
+    #endregion
+
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        rb.AddForce(transform.up * (acceleration * Input.GetAxis("Vertical")));
+        rb.AddForce(transform.right * (acceleration * Input.GetAxis("Horizontal")));
+
+        // Force max speed limit
+        if (rb.velocity.magnitude > maxSpeed)
+        {
+            rb.velocity = rb.velocity.normalized * maxSpeed;
+        }
+        currentSpeed = rb.velocity.magnitude;
+
+    }
+}
