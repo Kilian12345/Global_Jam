18ae3240a2dda509e669e01e468600fc0b3e7d05
dEFAULT <allairekilian@gmail.com>
2019-01-27 00:40:04 +0100

    Objectif_10%

---
+++ Neon/Assets/Map/Script/Point_Manager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Point_Manager : MonoBehaviour
+{
+    /*[SerializeField]
+    GameObject[] points;
+
+   void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider2D collider)
+    {
+        if (points.collider.gameObject.tag == "Player")
+        {
+            Debug.Log("Ok c'est bon");
+        }
+    }
+
+*/
+}
